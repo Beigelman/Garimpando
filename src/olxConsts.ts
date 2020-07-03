fa841108b35c47357f7dba0ef351053cabc01498
Daniel Beigelman <danielbeigelman@Macs-MacBook-Pro.local>
2020-07-02 21:50:36 -0300

    Deletando consts

--- src/olxConsts.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  input: "input.sc-1ppbg7c-1.jfvJlv",
-  button: "button",
-  url: "https://df.olx.com.br/",
-  li: ".sc-1fcmfeb-2.ggOGTJ",
-  name: "h2.fnmrjs-10.deEIZJ",
-  price: "p.fnmrjs-16.jqSHIm",
-};
